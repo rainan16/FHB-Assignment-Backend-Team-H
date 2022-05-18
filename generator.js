e46b0b5d5158df821bbb6a341a99c1aa80b590a0
rainan16 <27016852+rainan16@users.noreply.github.com>
2022-05-18 20:55:46 +0200

    fix eslint errors

--- generator.js
+++ generator.js
@@ -1,8 +1,8 @@
-function generateId(notes) {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-    return maxId + 1;
-  }
+function generateId (notes) {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
 
-module.exports = generateId;
+module.exports = generateId
